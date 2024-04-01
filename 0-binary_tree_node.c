6531b0e03fd01db7ae58e8869045a6e0bf266471
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-02 01:57:18 +0200

    my binary trr start

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,6 +3,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *ptr = malloc(sizeof(binary_tree_t));
+    if (ptr==NULL)
+        return NULL ;
     ptr->parent=parent;
     ptr->n=value;
     ptr->left=NULL;
