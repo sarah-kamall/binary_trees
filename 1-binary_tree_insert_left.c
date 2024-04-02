19a1f05ad503b80ac4fa6e28dbc47919d27c9083
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-02 02:33:32 +0200

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    binary_tree_t *newleft;
+    binary_tree_t *tmp;
+    
     if (!parent)
         return NULL;
-    binary_tree_t *tmp=NULL;
+    tmp=NULL;
     if (parent->left)
     {
         tmp=parent->left;
     }
-    binary_tree_t *newleft=binary_tree_node(parent, value);
+    newleft=binary_tree_node(parent, value);
     if (!newleft)
         return NULL;
     parent->left= newleft;
